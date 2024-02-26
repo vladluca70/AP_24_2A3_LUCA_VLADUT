fa17e263f1a9f9cd6189a2781af0460e8340c36a
vladluca70 <154993655+vladluca70@users.noreply.github.com>
2024-02-26 11:21:03 +0200

    Add files via upload

---
+++ Laborator_1/Compulsory/Compulsory.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Compulsory {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        String []languages= {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
+        int n=(int)(Math.random()*1_000_000);
+        n=n*3;
+        n=n+21; //10101 binary=21
+        n=n+255; //FF hexadecimal =255
+        n=n*6;
+        while(n>10)
+        {
+            int copie=n;
+            int suma=0;
+            int ultimaCifra;
+
+            while (copie!=0) {
+                ultimaCifra=copie%10;
+                copie=copie/10;
+                suma=suma+ultimaCifra;
+            }
+            n=suma;
+        }
+
+            System.out.println("Willy-nilly, this semester I will learn "+ languages[n]);
+        
+    }
+}
